9f81f5294fa0f3527eb603f11e956d2627f9ae43
Klem <klem86@protonmail.com>
2025-11-13 15:08:19 +0100

    Inject Metamask

--- dapp/src/providers/RainbowKitAndWagmiProvider.tsx
+++ dapp/src/providers/RainbowKitAndWagmiProvider.tsx
@@ -1,46 +1,39 @@
 'use client';
 import '@rainbow-me/rainbowkit/styles.css';
 
-import {
-  getDefaultConfig,
-  RainbowKitProvider,
-  darkTheme 
-} from '@rainbow-me/rainbowkit';
-import { WagmiProvider } from 'wagmi';
-import {
-  sepolia
-} from 'wagmi/chains';
-import {
-  QueryClientProvider,
-  QueryClient,
-} from "@tanstack/react-query";
+import {getDefaultConfig, RainbowKitProvider, darkTheme} from '@rainbow-me/rainbowkit';
+import {createConfig, injected, WagmiProvider} from 'wagmi';
+import {sepolia} from 'wagmi/chains';
+import {QueryClientProvider, QueryClient,} from "@tanstack/react-query";
+import {http} from "viem";
 
-const config = getDefaultConfig({
-  appName: 'My RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
-  chains: [sepolia],
-  ssr: true, // If your dApp uses server side rendering (SSR)
+export const config = createConfig({
+    chains: [sepolia],
+    connectors: [injected({ target: 'metaMask' })],
+    transports: { [sepolia.id]: http() },
+    ssr: true
 });
+
 
 const queryClient = new QueryClient();
 
-const RainbowKitAndWagmiProvider = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider
-            theme={darkTheme({
-            accentColor: '#7b3fe4',
-            accentColorForeground: 'white',
-            borderRadius: 'small',
-            fontStack: 'system',
-            overlayBlur: 'small',
-            })}>
-          {children}
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
-  );
+const RainbowKitAndWagmiProvider = ({children}: { children: React.ReactNode }) => {
+    return (
+        <WagmiProvider config={config}>
+            <QueryClientProvider client={queryClient}>
+                <RainbowKitProvider
+                    theme={darkTheme({
+                        accentColor: '#7b3fe4',
+                        accentColorForeground: 'white',
+                        borderRadius: 'small',
+                        fontStack: 'system',
+                        overlayBlur: 'small',
+                    })}>
+                    {children}
+                </RainbowKitProvider>
+            </QueryClientProvider>
+        </WagmiProvider>
+    );
 };
 
 export default RainbowKitAndWagmiProvider;
